da73eeeb542393d7e93ad61a84b7807d45ce02aa
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-06-14 14:37:46 +0800

    feat: test upload NAS

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -1,6 +1,6 @@
-<script setup lang="ts">
-  const handleUpload = async (event: any) => {
-    const file = event?.target?.files[0]
+export default eventHandler(async (event) => {
+  try {
+    const formData = await readFormData(event)
 
     const url =
       "http://103.170.89.96:5000/webapi/entry.cgi?_sid=Koh_UwJUmdr5ULPj-TDHWxIfGkiMDJpV4Y9xjRcEU3cv4dPwwWKbcB_ogtn0llz1BtdXSiS_GoBendVMRfNkts"
@@ -12,27 +12,22 @@
       path: "/homes/nas_diskominfotik/FTP/angga",
       create_parents: "true",
       overwrite: "true",
-      file,
     }
 
-    const formData = new FormData()
-    for (const [key, value] of Object.entries(body)) {
+    for (const [key, value] of Object.entries(body) as any) {
       formData.append(key, value)
     }
 
-    const result = await $fetch(url, {
-      method: "post",
+    console.log(formData)
+
+    return $fetch(url, {
       body: formData,
+      method: "POST",
     })
-
-    console.log({ result })
+    return {
+      message: "ok",
+    }
+  } catch (error: any) {
+    throw createError(error)
   }
-</script>
-
-<template>
-  <div>
-    <form>
-      <input name="upload" type="file" @input="handleUpload" />
-    </form>
-  </div>
-</template>
+})
